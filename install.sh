785b91633cd39d058be33bc873e26f2b1b685afc
r3dg0d <r3dg0d@archlinux.local>
2026-01-02 14:51:51 -0800

    Fix vconsole.conf error by creating the file during installation
    - Add /etc/vconsole.conf with US keyboard layout and readable font
    - Prevents 'File not found /etc/vconsole.conf' error during install

--- install.sh
+++ install.sh
@@ -139,6 +139,8 @@ hwclock --systohc
 echo "en_US.UTF-8 UTF-8" > /etc/locale.gen
 locale-gen
 echo "LANG=en_US.UTF-8" > /etc/locale.conf
+echo "KEYMAP=us" > /etc/vconsole.conf
+echo "FONT=ter-132n" >> /etc/vconsole.conf
 echo "$HOSTNAME" > /etc/hostname
 echo "127.0.0.1 localhost" >> /etc/hosts
 echo "127.0.1.1 $HOSTNAME.localdomain $HOSTNAME" >> /etc/hosts
